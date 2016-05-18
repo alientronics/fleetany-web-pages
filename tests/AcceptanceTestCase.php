7781f44b932626eb0aa32f15a108582b1d85bb35
Rafael <rafaelpviegas@gmail.com>
2016-05-18 04:39:13 -0300

    Tentativa de correção dos testes

--- tests/AcceptanceTestCase.php
+++ tests/AcceptanceTestCase.php
@@ -20,25 +20,9 @@ class AcceptanceTestCase extends BaseTestCase
      * @return \Illuminate\Foundation\Application
      */
 
-    public function setUp()
-    {
-        parent::setUp();
-        \DB::connection()->enableQueryLog();
-    }
-
-    public function tearDown()
-    {
-        echo ' ' . count(\DB::getQueryLog()) . ' queries in '
-            . (array_sum(array_column(\DB::getQueryLog(), 'time'))/1000) . ' seconds';
-        //var_dump( \DB::getQueryLog() );
-        parent::tearDown();
-    }
-
     public function createApplication()
     {
         $app = require __DIR__.'/../vendor/alientronics/fleetany-web/bootstrap/app.php';
-
-        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
     }
